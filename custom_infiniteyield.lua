0cd3cc4f8750ad864b9582f80fd7337e6f6049d0
Thanakorn <68836066+KiwwyIsDev@users.noreply.github.com>
2025-10-29 01:24:54 +0700

    feat: change queueteleport url

--- custom_infiniteyield.lua
+++ custom_infiniteyield.lua
@@ -6463,7 +6463,7 @@ local TeleportCheck = false
 Players.LocalPlayer.OnTeleport:Connect(function(State)
 	if KeepInfYield and (not TeleportCheck) and queueteleport then
 		TeleportCheck = true
-		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
+		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/KiwwyIsDev/free-script/refs/heads/main/custom_infiniteyield.lua'))()")
 	end
 end)
 
